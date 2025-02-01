7465c4f9ac22a010a6cee24938d3bc7ea9156757
Robert <robertmaio@outlook.com>
2025-02-01 12:10:32 -0300

    eu odeio front part 2

--- src/Ux/index.js
+++ src/Ux/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./Pages/Main/App.js";
+import App from "./src/Pages/Main/App.js";
 import "./styles.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
